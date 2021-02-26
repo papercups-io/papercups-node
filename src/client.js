94722ba6df03fb475a124b14091c0a20449e0c61
Alex Reichert <reichertjalex@gmail.com>
2021-02-26 10:36:20 -0500

    Basic API endpoints v1.0.1 (#1)
    * Set up conversations, customers, and messages APIs
    
    * Fix whitespace
    
    * 1.0.1

--- src/client.js
+++ src/client.js
@@ -10,7 +10,108 @@ const client = (token = process.env.PAPERCUPS_API_KEY, options = {}) => {
       .then((res) => res.body.data);
   };
 
-  return {me};
+  const conversations = {
+    list: async (query = {}) => {
+      return request
+        .get(`${host}/api/v1/conversations`)
+        .set('Authorization', `Bearer ${token}`)
+        .query(query)
+        .then((res) => res.body.data);
+    },
+    retrieve: async (id) => {
+      return request
+        .get(`${host}/api/v1/conversations/${id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .then((res) => res.body.data);
+    },
+    create: async (conversation) => {
+      return request
+        .post(`${host}/api/v1/conversations`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({conversation})
+        .then((res) => res.body.data);
+    },
+    update: async (id, updates) => {
+      return request
+        .put(`${host}/api/v1/conversations/${id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({conversation: updates})
+        .then((res) => res.body.data);
+    },
+    destroy: async (id) => {
+      return request
+        .del(`${host}/api/v1/conversations/${id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .then((res) => res.body);
+    },
+  };
+
+  const customers = {
+    list: async (query = {}) => {
+      return request
+        .get(`${host}/api/v1/customers`)
+        .set('Authorization', `Bearer ${token}`)
+        .query(query)
+        .then((res) => res.body.data);
+    },
+    retrieve: async (id) => {
+      return request
+        .get(`${host}/api/v1/customers/${id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .then((res) => res.body.data);
+    },
+    create: async (customer) => {
+      return request
+        .post(`${host}/api/v1/customers`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({customer})
+        .then((res) => res.body.data);
+    },
+    update: async (id, updates) => {
+      return request
+        .put(`${host}/api/v1/customers/${id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({customer: updates})
+        .then((res) => res.body.data);
+    },
+    destroy: async (id) => {
+      return request
+        .del(`${host}/api/v1/customers/${id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .then((res) => res.body);
+    },
+  };
+
+  const messages = {
+    list: async (query = {}) => {
+      return request
+        .get(`${host}/api/v1/messages`)
+        .set('Authorization', `Bearer ${token}`)
+        .query(query)
+        .then((res) => res.body.data);
+    },
+    retrieve: async (id) => {
+      return request
+        .get(`${host}/api/v1/messages/${id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .then((res) => res.body.data);
+    },
+    create: async (message) => {
+      return request
+        .post(`${host}/api/v1/messages`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({message})
+        .then((res) => res.body.data);
+    },
+    destroy: async (id) => {
+      return request
+        .del(`${host}/api/v1/messages/${id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .then((res) => res.body);
+    },
+  };
+
+  return {me, conversations, customers, messages};
 };
 
 module.exports = client;
